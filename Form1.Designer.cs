55cf1be2e0f78cd21992f283010828a29a7842de
MarkoParac <46823824+MarkoParac@users.noreply.github.com>
2020-02-14 16:02:29 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnKreni = new System.Windows.Forms.Button();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(98, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 29;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox9.Location = new System.Drawing.Point(295, 324);
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(90, 90);
+            this.textBox9.TabIndex = 28;
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox8.Location = new System.Drawing.Point(199, 324);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(90, 90);
+            this.textBox8.TabIndex = 27;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox7.Location = new System.Drawing.Point(103, 324);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(90, 90);
+            this.textBox7.TabIndex = 26;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox6.Location = new System.Drawing.Point(295, 228);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(90, 90);
+            this.textBox6.TabIndex = 25;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox5.Location = new System.Drawing.Point(199, 228);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(90, 90);
+            this.textBox5.TabIndex = 24;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox4.Location = new System.Drawing.Point(103, 228);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(90, 90);
+            this.textBox4.TabIndex = 23;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox3.Location = new System.Drawing.Point(295, 132);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(90, 90);
+            this.textBox3.TabIndex = 22;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox2.Location = new System.Drawing.Point(199, 132);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(90, 90);
+            this.textBox2.TabIndex = 21;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox1.Location = new System.Drawing.Point(103, 132);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(90, 90);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // btnKreni
+            // 
+            this.btnKreni.Location = new System.Drawing.Point(268, 20);
+            this.btnKreni.Name = "btnKreni";
+            this.btnKreni.Size = new System.Drawing.Size(125, 52);
+            this.btnKreni.TabIndex = 19;
+            this.btnKreni.Text = "Kreni";
+            this.btnKreni.UseVisualStyleBackColor = true;
+            this.btnKreni.Click += new System.EventHandler(this.btnKreni_Click);
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(98, 52);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 20);
+            this.txtY.TabIndex = 18;
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(98, 20);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 20);
+            this.txtX.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Y";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 441);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnKreni);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnKreni;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKreni_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Igra: " + txtX.Text;
+            Ocisti();
+        }
+        
+        private void Ocisti()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c.GetType() == typeof(TextBox) && c.Name != "txtX" && c.Name != "txtY")
+                {
+                    c.Text = "";
+                }
+            }
+        }
+
+        bool x = true;
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            //if(((TextBox)sender).Text != "")
+            //{
+            //    MessageBox.Show("Polje je već odabrano!");
+            //}
+            //else
+            //{
+            //    if(x == true)
+            //    {
+            //        ((TextBox)sender).Text = "X";
+            //        label4.Text = "Igra: " + txtY.Text;
+            //    }
+            //    else
+            //    {
+            //        ((TextBox)sender).Text = "O";
+            //        label4.Text = "Igra: " + txtX.Text;
+            //    }
+            //}
+
+            //x = !x;
+            //Provjera();
+        }
+
+       private void Provjera()
+        {
+            if (textBox1.Text == "X" && textBox2.Text == "X" && textBox3.Text == "X")
+            {
+                MessageBox.Show("Pobjedio je: " + txtX.Text);
+            }
+            if (textBox4.Text == "X" && textBox5.Text == "X" && textBox6.Text == "X")
+            {
+                MessageBox.Show("Pobjedio je: " + txtX.Text);
+            }
+            if (textBox7.Text == "X" && textBox8.Text == "X" && textBox9.Text == "X")
+            {
+                MessageBox.Show("Pobjedio je: " + txtX.Text);
+            }
+
+            if (textBox1.Text == "O" && textBox2.Text == "O" && textBox2.Text == "O")
+            {
+                MessageBox.Show("Pobjedio je: " + txtY.Text);
+            }
+            if (textBox4.Text == "O" && textBox5.Text == "O" && textBox6.Text == "O")
+            {
+                MessageBox.Show("Pobjedio je: " + txtY.Text);
+            }
+            if (textBox7.Text == "O" && textBox8.Text == "O" && textBox9.Text == "O")
+            {
+                MessageBox.Show("Pobjedio je: " + txtY.Text);
+            }
+
+            if (textBox1.Text == "X" && textBox4.Text == "X" && textBox7.Text == "X")
+            {
+                MessageBox.Show("Pobjedio je: " + txtX.Text);
+            }
+            if (textBox2.Text == "X" && textBox5.Text == "X" && textBox8.Text == "X")
+            {
+                MessageBox.Show("Pobjedio je: " + txtX.Text);
+            }
+            if (textBox3.Text == "X" && textBox6.Text == "X" && textBox9.Text == "X")
+            {
+                MessageBox.Show("Pobjedio je: " + txtX.Text);
+            }
+
+            if (textBox1.Text == "O" && textBox4.Text == "O" && textBox7.Text == "O")
+            {
+                MessageBox.Show("Pobjedio je: " + txtY.Text);
+            }
+            if (textBox2.Text == "O" && textBox5.Text == "O" && textBox8.Text == "O")
+            {
+                MessageBox.Show("Pobjedio je: " + txtY.Text);
+            }
+            if (textBox3.Text == "O" && textBox6.Text == "O" && textBox9.Text == "O")
+            {
+                MessageBox.Show("Pobjedio je: " + txtY.Text);
+            }
+
+            if (textBox3.Text == "O" && textBox5.Text == "O" && textBox7.Text == "O")
+            {
+                MessageBox.Show("Pobjedio je: " + txtY.Text);
+            }
+
+            if (textBox1.Text == "O" && textBox5.Text == "O" && textBox9.Text == "O")
+            {
+                MessageBox.Show("Pobjedio je: " + txtY.Text);
+            }
+
+            if (textBox3.Text == "X" && textBox5.Text == "X" && textBox7.Text == "X")
+            {
+                MessageBox.Show("Pobjedio je: " + txtX.Text);
+            }
+
+            if (textBox1.Text == "X" && textBox5.Text == "X" && textBox9.Text == "X")
+            {
+                MessageBox.Show("Pobjedio je: " + txtX.Text);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if ((c as TextBox) != null)
+                {
+                    (c as TextBox).Click += Form1_Click;
+                }
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (((TextBox)sender).Text != "")
+            {
+                MessageBox.Show("Polje je već odabrano!");
+            }
+            else
+            {
+                if (x == true)
+                {
+                    ((TextBox)sender).Text = "X";
+                    label4.Text = "Igra: " + txtY.Text;
+                }
+                else
+                {
+                    ((TextBox)sender).Text = "O";
+                    label4.Text = "Igra: " + txtX.Text;
+                }
+            }
+
+            x = !x;
+            Provjera();
+        }
+    }
+}
